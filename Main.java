526e742d0fe9c4669e7d1626d7142be2553ba403
Igor Tchone Riba <132633397+IgorTchone@users.noreply.github.com>
2024-12-21 19:18:43 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        do {
+            System.out.println("Number: " + number);
+            number++;
+        } while (number <= 5);
+    }
+}
